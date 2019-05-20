287b8f41a238bece0d16c4b2bd62671710f5237f
Anjali <anjali@code.org>
2019-05-20 14:55:42 -0700

    checkpoint: set animation

--- apps/src/gamelab/Spritelab.js
+++ apps/src/gamelab/Spritelab.js
@@ -2,16 +2,44 @@ var Spritelab = function() {
   console.log('spritelab!');
   this.p5 = null;
   window.p5.prototype.spriteId = 0;
+  window.p5.prototype.sprites = {};
 
-  window.p5.prototype.makeNewSprite = function(animation, x, y) {
+  window.p5.prototype.setAnimation = function(spriteIndex, animation) {
+    let sprite = this.sprites[spriteIndex];
+    if (sprite) {
+      sprite.setAnimation(animation);
+      sprite.scale /= sprite.baseScale;
+      sprite.baseScale =
+        100 /
+        Math.max(
+          100,
+          sprite.animation.getHeight(),
+          sprite.animation.getWidth()
+        );
+      sprite.scale *= sprite.baseScale;
+    }
+  };
+
+  window.p5.prototype.makeSpriteNative = function(animation, x, y) {
     var sprite = this.createSprite(x, y);
-    sprite.id = this.spriteId;
+    this.sprites[this.spriteId] = sprite; // lookup sprite given id
+    sprite.id = this.spriteId; // lookup id given sprite
+    sprite.baseScale = 1;
+    if (animation) {
+      this.setAnimation(sprite.id, animation);
+    }
+    sprite.speed = 10;
+    sprite.patrolling = false;
+    sprite.things_to_say = [];
+    sprite.behaviors = [];
+    sprite.collidable = false;
+    sprite.collisionObjects = [];
+    sprite.isGroup = false;
     this.spriteId++;
     return sprite.id;
   };
 
   window.p5.prototype.executeDrawLoopAndCallbacks = function() {
-    console.log('native draw loop!');
     this.drawSprites();
   };
 };
--- dashboard/config/libraries/NativeSpriteLab.interpreted.js
+++ dashboard/config/libraries/NativeSpriteLab.interpreted.js
@@ -1,4 +1,12 @@
+function makeNewSprite(animation, x, y) {
+  var spriteIndex = makeSpriteNative(x, y);
+  return spriteIndex;
+}
+
+function makeNewSpriteAnon(animation, location) {
+  makeSpriteNative(animation, location.x, location.y);
+}
+
 function draw() {
-  console.log('interpreter draw');
   executeDrawLoopAndCallbacks();
 }
