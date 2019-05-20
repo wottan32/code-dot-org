e549208fb1e5740271c2b53ad8e0b992120f6c2a
Anjali <anjali@code.org>
2019-05-20 16:52:41 -0700

    checkpoint: single or group for setting animation
    Also changes animation dropdown block to generate just the costume name as a string, not allSpritesWithAnimation({animation})

--- apps/src/gamelab/Spritelab.js
+++ apps/src/gamelab/Spritelab.js
@@ -1,29 +1,57 @@
 var Spritelab = function() {
   console.log('spritelab!');
   this.p5 = null;
-  window.p5.prototype.spriteId = 0;
-  window.p5.prototype.sprites = {};
+
+  var spriteId = 0;
+  var sprites = {};
+
+  var allSpritesWithAnimation = function(animationName) {
+    let group = [];
+    Object.keys(sprites).forEach(s => {
+      if (sprites[s].animationName === animationName) {
+        group.push(sprites[s]);
+      }
+    });
+    return group;
+  };
+
+  var singleOrGroup = function(spriteOrGroup, func, args) {
+    if (typeof spriteOrGroup === 'number') {
+      // sprite referenced by index
+      const sprite = sprites[spriteOrGroup];
+      return [sprite];
+    }
+    if (typeof spriteOrGroup === 'string') {
+      // group referenced by costume name
+      const spriteGroup = allSpritesWithAnimation(spriteOrGroup);
+      return spriteGroup;
+    }
+    console.log('unknown type of spriteOrGroup: ' + typeof spriteOrGroup);
+  };
 
   window.p5.prototype.setAnimation = function(spriteIndex, animation) {
-    let sprite = this.sprites[spriteIndex];
-    if (sprite) {
-      sprite.setAnimation(animation);
-      sprite.scale /= sprite.baseScale;
-      sprite.baseScale =
-        100 /
-        Math.max(
-          100,
-          sprite.animation.getHeight(),
-          sprite.animation.getWidth()
-        );
-      sprite.scale *= sprite.baseScale;
+    let sprites = singleOrGroup(spriteIndex);
+    if (sprites) {
+      sprites.forEach(sprite => {
+        sprite.setAnimation(animation);
+        sprite.animationName = animation;
+        sprite.scale /= sprite.baseScale;
+        sprite.baseScale =
+          100 /
+          Math.max(
+            100,
+            sprite.animation.getHeight(),
+            sprite.animation.getWidth()
+          );
+        sprite.scale *= sprite.baseScale;
+      });
     }
   };
 
   window.p5.prototype.makeSpriteNative = function(animation, x, y) {
     var sprite = this.createSprite(x, y);
-    this.sprites[this.spriteId] = sprite; // lookup sprite given id
-    sprite.id = this.spriteId; // lookup id given sprite
+    sprites[spriteId] = sprite; // lookup sprite given id
+    sprite.id = spriteId; // lookup id given sprite
     sprite.baseScale = 1;
     if (animation) {
       this.setAnimation(sprite.id, animation);
@@ -35,7 +63,7 @@ var Spritelab = function() {
     sprite.collidable = false;
     sprite.collisionObjects = [];
     sprite.isGroup = false;
-    this.spriteId++;
+    spriteId++;
     return sprite.id;
   };
 
